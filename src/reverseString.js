855c215ea4b6c4e643ad456fbf77f96a896739ef
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-02-21 14:42:31 +0000

    Implement reverseString tests and function

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,3 +1,7 @@
-function reverseString(str) {}
+function reverseString(str) {
+  if (typeof str === 'object')
+    throw new Error('Argument was an object. Provide a string-able value.');
+  return Array.from(str.toString()).reverse().join('');
+}
 
 module.exports = reverseString;
