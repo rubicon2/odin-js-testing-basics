650cef48f91c3172ec163d51675294e9e3802346
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-02-21 14:42:31 +0000

    Implement reverseString tests and function

--- src/reverseString.test.js
+++ src/reverseString.test.js
@@ -3,3 +3,27 @@ const reverseString = require('./reverseString');
 test('reverse a string', () => {
   expect(reverseString('string')).toBe('gnirts');
 });
+
+test('reverse a string of length 1', () => {
+  expect(reverseString('a')).toBe('a');
+});
+
+test('return a string of length zero unchanged', () => {
+  expect(reverseString('')).toBe('');
+});
+
+test('if provided a number, convert to string then reverse', () => {
+  expect(reverseString(987)).toBe('789');
+});
+
+test('if provided an object, throw an error', () => {
+  expect(() =>
+    reverseString({ firstname: 'billy', surname: 'borgan' }),
+  ).toThrow();
+});
+
+test('if provided an array, throw an error', () => {
+  expect(() => {
+    reverseString(['0', '1', '2']).toThrow();
+  });
+});
